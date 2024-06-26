22095abab884efb15a002db579ec4f328d7e7623
Kimuli Arnold <kiflower35@gmail.com>
2024-06-26 09:55:16 -0700

    added to app.py

--- backend/app.py
+++ backend/app.py
@@ -3,5 +3,11 @@ from flask import Flask, request, jsonify
 # Initialise the flask app
 app = Flask(__name__)
 
-@app.route("/")
 
+@app.route("/")
+def home():
+    return "Welcome to the Flask API"
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
